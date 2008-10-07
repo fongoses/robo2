f0cefeb74fc1ceaa45171cf55b1cb8e70aeb2d1d
heitor.polidoro <heitor.polidoro@88984ef6-8ff3-11dd-a371-4df34741e537>
2008-10-07 17:04:11 +0000

    Inicio da classe Robo

---
+++ source/robo.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  robo.cpp
+ *
+ *    Description:  Descricao da classe Robo
+ *
+ *        Version:  1.0
+ *        Created:  10/07/2008 02:02:32 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Heitor Luis Polidoro (hlp), heitor@icmc.usp.br
+ *        Company:  ICMC - USP São Carlos
+ *
+ * =====================================================================================
+ */
+
+
