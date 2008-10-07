652221f09a961bb1deafb847641b59696dbc2043
heitor.polidoro <heitor.polidoro@88984ef6-8ff3-11dd-a371-4df34741e537>
2008-10-07 15:10:17 +0000

    Dijkstra funfando

--- trunk/source/main.cpp
+++ trunk/source/main.cpp
@@ -27,30 +27,36 @@ using namespace std;
 main ( int argc, char *argv[] )
 {
 	Mapa M;
+	ListaVertices caminho;
+	ListaVertices::iterator it_c;
 
 
-	M.adicionarVertice(1, *(new Ponto(1, 2)));
-	M.adicionarVertice(2, *(new Ponto(2, 2)));
-	M.adicionarVertice(3, *(new Ponto(3, 2)));
-	M.adicionarVertice(4, *(new Ponto(4, 2)));
-	M.adicionarVertice(5, *(new Ponto(5, 2)));
-	M.adicionarVertice(6, *(new Ponto(6, 2)));
+
+	M.adicionarVertice(1, *(new Ponto(0, 1)));
+	M.adicionarVertice(2, *(new Ponto(1, 2)));
+	M.adicionarVertice(3, *(new Ponto(1, 0)));
+	M.adicionarVertice(4, *(new Ponto(2, 2)));
+	M.adicionarVertice(5, *(new Ponto(2, 0)));
+	M.adicionarVertice(6, *(new Ponto(3, 1)));
 
 
 
 	M.adicionarAresta(1, 2);
 	M.adicionarAresta(1, 3);
 	M.adicionarAresta(1, 5);
+	M.adicionarAresta(1, 6);
 	M.adicionarAresta(2, 4);
-	M.adicionarAresta(2, 5);
 	M.adicionarAresta(3, 4);
-
-	if(M.adjacente(1, 2)) cout << "1 adjacente ao 2.\n";
-	else cout << "1 não adjacente ao 2.\n";
-	if(M.adjacente(1, 4)) cout << "1 adjacente ao 4.\n";
-	else cout << "1 não adjacente ao 4.\n";
+	M.adicionarAresta(4, 6);
+	M.adicionarAresta(5, 6);
 
 	M.imprimir();
+
+	caminho = M.dijkstra(1, 6);
+	cout << "Caminho: ";
+	for(it_c = caminho.begin(); it_c != caminho.end(); it_c++)
+		cout << *it_c << " ";
+	cout << endl;
 
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
